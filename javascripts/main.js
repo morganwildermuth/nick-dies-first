225c667add331167023dba17ace505487632f9c9
srinivas rao <rao@srinivas-raos-macbook-4.local>
2013-11-05 23:23:09 -0800

    moves controller to separate controller.js file

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,28 +7,3 @@ function bindPlayerInput() {
   $(".new-player").submit(Controller.addPlayer)
 };
 
-var Controller = {
-
-  togglePartials: function($partialToShow) {
-    Views.page().empty()
-    Views.page().append($partialToShow)
-  },
-
-  showPartial: function($partial) {
-    Views.page().append($partial)
-  },
-
-  addPlayer: function( event ) {
-    event.preventDefault()
-
-    var waiting = Views.waiting().clone()
-    Controller.togglePartials(waiting)
-
-    var playerName = event.target.player.value
-    Sync.addPlayer(playerName);
-
-    if(Game.checkForPlayers()){
-      Game.setNonCivilianRoles();
-    }
-  }
-}
