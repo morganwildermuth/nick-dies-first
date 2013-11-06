fd5313a1164af0b2c274d2df13003562094a6651
Apprentice <apprentice@dbc-31.local>
2013-11-05 16:59:11 -0800

    Game is aware of players

--- javascripts/sync.js
+++ javascripts/sync.js
@@ -1,6 +1,13 @@
 var Sync = {
-  addPlayer: function(name){
+  addPlayer: function(name) {
     var playerRef = new Firebase('https://mafia.firebaseIO.com/' + name)
     playerRef.set('civilian')
+  },
+
+  getPlayers: function() {
+    var playerRef = new Firebase('https://mafia.firebaseIO.com/')
+    playerRef.on('value', function(snapshot) {
+      Game.players = snapshot.val()
+    })
   }
 }
