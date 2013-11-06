49b9b8245456f26203e88f98132d44c5b0b6792e
Apprentice <apprentice@dbc-31.local>
2013-11-05 17:08:02 -0800

    refactor sync.js

--- javascripts/sync.js
+++ javascripts/sync.js
@@ -1,11 +1,13 @@
 var Sync = {
+  url: 'https://mafia.firebaseIO.com/',
+
   addPlayer: function(name) {
-    var playerRef = new Firebase('https://mafia.firebaseIO.com/' + name)
+    var playerRef = new Firebase(this.url + name)
     playerRef.set('civilian')
   },
 
   getPlayers: function() {
-    var playerRef = new Firebase('https://mafia.firebaseIO.com/')
+    var playerRef = new Firebase(this.url)
     playerRef.on('value', function(snapshot) {
       Game.players = snapshot.val()
     })
