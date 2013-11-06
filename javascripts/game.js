8425e6f9e804e6573774426b7ba511c7a2d60257
Apprentice <apprentice@dbc-31.local>
2013-11-05 19:22:27 -0800

    find random civilians to assign new roles to

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,2 +1,36 @@
 var Game = {
+  expectedPlayers: 6,
+
+  currentPlayers: function() {
+    return Object.keys(Game.players)
+  },
+
+  checkForPlayers: function() {
+    if (this.currentPlayers.length === this.expectedPlayers){
+      Game.setRoles()
+    }
+  },
+
+  chooseRandomPlayer: function(){
+    index = Math.floor((Math.random()*this.currentPlayers().length));
+    return this.currentPlayers()[index]
+  },
+
+  chooseRandomCivilian: function() {
+    var randomPlayerName = this.chooseRandomPlayer()
+    if (this.players[randomPlayerName] != 'civilian'){
+      Game.chooseRandomCivilian()
+    }
+    return randomPlayerName
+  },
+
+  setNonCivilianRoles: function() {
+    this.setPolice()
+    // set 2 mafia, 1 doctor, 1 police
+  },
+
+  setPolice: function() {
+    playerName = this.chooseRandomCivilian()
+    updateRole(playerName, 'police')
+  }
 }
